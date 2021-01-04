3f07ab9a7ec1cb645c118dda48d3681c3034ac51
Your Name <flemming@northtech.dk>
2021-01-04 19:20:27 +0100

    initial message

---
+++ Hashing/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hashing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private byte[] _key;
+        private byte[] _hashedKey;
+        private byte[] _hvac;
+
+        private readonly HashingSupervisor _hashingSupervisor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _key = new byte[] { };
+            _hashedKey = new byte[] { };
+            _hashingSupervisor = new HashingSupervisor();
+            //_hashingSupervisor.ConvertTestToSHA256();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(UserInputKeyText.Text))
+            {
+                MessageBox.Show("You must Create a Key!");
+            }
+            else
+            {
+                // This key is a byte array of the text input by the user
+                _key = _hashingSupervisor.GetBytes(UserInputKeyText.Text);
+                // go and get the key hashed after and returns it to the hashed key array
+                _hashedKey = _hashingSupervisor.GetHashedKey(HashAlgorithmComboBox.SelectedIndex, _key);
+                // reverts it to plain text and writes it to th Text field
+                HashedKeyText.Text = _hashingSupervisor.GetBytesToPlainText(_hashedKey);
+            }
+        }
+
+        private void Hvac_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(HashedKeyText.Text) || string.IsNullOrEmpty(UserInputMessageText.Text))
+            {
+                MessageBox.Show("You must create a key and type in a message");
+            }
+            else
+            {
+                byte[] message = _hashingSupervisor.GetBytes(UserInputMessageText.Text);
+                _hvac = _hashingSupervisor.GenerateHmacMessage(_key, message);
+                HmacMessageText.Text = _hashingSupervisor.GetBytesToPlainText(_hvac);
+                HmacMessageHex.Text = _hashingSupervisor.GetByteArrayToHexString(_hvac);
+            }
+
+            //if (string.IsNullOrEmpty(InputMessageText.Text) || string.IsNullOrEmpty(HashedKeyText.Text))
+            //{
+            //    MessageBox.Show("You must generate a key and a message");
+            //}
+            //else
+            //{
+
+
+            //   // HvacMessageText.Text= _hashingSupervisor.GetHashedKey()
+            //}
+        }
+    }
+}
